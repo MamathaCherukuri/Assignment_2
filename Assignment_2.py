79f4cec0e11a80a3c2795203f31b88aadbf27e30
MamathaCherukuri <mamatha.mba26@gmail.com>
2019-01-07 14:55:38 +0530

    Add files via upload

---
+++ Assignment_2.py
@@ -0,0 +1,229 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# 1Q. Write a program which accepts a sequence of comma-separated numbers from console and
+# generate a list.
+
+# In[8]:
+
+
+csv_list=input("Enter raw values:")
+l=csv_list.split(",")
+print(list(l))
+
+
+# 2Q.'''Create the below pattern using nested for loop in Python.'''
+
+# In[8]:
+
+
+for i in range(0, 4):
+    for j in range(0, i+1):
+        print('* ',end="")
+    print()
+
+for i in range(5,0,-1):
+    for j in range(i):
+        print('* ', end="")
+    print('')
+
+#3Q Write a Python program to reverse a word after accepting the input from the user.
+# In[10]:
+
+
+word = input("Input a word to reverse: ")
+
+for char in range(len(word) - 1, -1, -1):
+    print(word[char], end="")
+print("\n")
+
+
+# '''4Q. Write a Python Program to print the given string in the format specified in the sample output. WE, THE PEOPLE OF INDIA, having solemnly resolved to constitute India into a SOVEREIGN, SOCIALIST, SECULAR, DEMOCRATIC REPUBLIC and to secure to all its citizens'''
+
+# In[20]:
+
+
+print("WE, THE PEOPLE OF INDIA,\n\thaving solemnly resolved to constitute India into a SOVEREIGN,\n\t\tSOCIALIST, SECULAR, DEMOCRATIC REPUBLIC\nand to secure to all its citizens")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# 
+
+# 
+
+# 
+
+# 
+
+# 
+
+# 
+
+# 
+
+# 
+
+# 
+
+# 
